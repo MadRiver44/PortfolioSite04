f5545896814c659fb8d434ad5ff24eef9a634a5f
Kevin Turney <super996@optonline.net>
2018-04-26 11:08:28 -0400

    basic config, add style loaders, devServer, html and hmr plugins

---
+++ webpack.dev.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/,
+          use: [{
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'sass-loader'
+          }]
+        }]
+
+},
+  devServer: {
+    contentBase: 'build',
+    hot: true,
+    inline: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new webpack.HotModuleReplacementPlugin(),
+    ],
+}

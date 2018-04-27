20d54f78ed5f9b018c0440a881633ec070b4aa8e
Kevin Turney <super996@optonline.net>
2018-04-27 12:23:38 -0400

    add style start

---
+++ src/client/Components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import styles from '../../stylesheets/main.scss'
+
+const App = () => {
+  return (<div className="start">{"Hello Component hot"}</div>)
+}
+export default hot(module)(App)

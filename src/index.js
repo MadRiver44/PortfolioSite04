4956b044be83d99888ab12f991da6c5fe9a22e03
Kevin Turney <super996@optonline.net>
2018-04-27 12:26:47 -0400

    add seven folders and main.scss for style organization

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import styles from './stylesheets/main.scss'
 
-import App from './Components/App'
+import App from './client/Components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 

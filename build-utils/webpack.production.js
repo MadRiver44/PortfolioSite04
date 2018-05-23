1ad003ea660e51d957666a71b4d98eeda6afacba
Kevin Turney <super996@optonline.net>
2018-05-23 11:22:12 -0400

    comment out webpack namespace

--- build-utils/webpack.production.js
+++ build-utils/webpack.production.js
@@ -1,19 +1,19 @@
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = () => ({
   output: {
-    filename: "bundle.[chunkhash:8].js"
+    filename: 'bundle.[chunkhash:8].js',
   },
   module: {
     rules: [
-    {
-      test:/\.scss$/,
-      use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader']
-    }
-    ]
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
+});
 
-})
 // .css or .scss ???
-//plugins: [MiniCssExtractPlugin({ filename: '[name].scss', chunkFilename:'[id.scss' ]
+// plugins: [MiniCssExtractPlugin({ filename: '[name].scss', chunkFilename:'[id.scss' ]

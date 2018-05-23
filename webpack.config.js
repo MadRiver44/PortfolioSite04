04ea2c1a1e0cecccc4b24b3e75f9b428ac1b0767
Kevin Turney <super996@optonline.net>
2018-05-23 11:24:07 -0400

    add plugins commonly used both dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,46 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const presetConfig = require('./build-utils/loadPresets');
 const webpackMerge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = ({mode, presets} = {mode: 'production', presets: [] }) => {
-  return webpackMerge(
-  {
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) => {
+  return webpackMerge({
     entry: './src/index.js',
     mode,
     module: {
       rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      { test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: ['file-loader']
-      },
-      { test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
-      ]
+        {
+          test: /\.(js)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif|jpeg)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: ['file-loader'],
+        },
+      ],
     },
     plugins: [
       new HtmlWebpackPlugin({ template: 'src/index.html' }),
       new webpack.HotModuleReplacementPlugin(),
-    ]
-  })
-}
+    ],
+  });
+};

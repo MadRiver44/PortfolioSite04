55765d6ece5b2a1af7ae467074f567752a7a7c80
Kevin Turney <super996@optonline.net>
2018-05-23 11:23:16 -0400

    sample component

--- src/client/Components/App.js
+++ src/client/Components/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { hot } from 'react-hot-loader'
-import styles from '../../stylesheets/main.scss'
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import styles from '../../stylesheets/main.scss';
 
 const App = () => {
-  return (<div className="start">{"Hello Component hot"}</div>)
-}
-export default hot(module)(App)
+  return <div className="start">{'Hello Component hot'}</div>;
+};
+export default hot(module)(App);

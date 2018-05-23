50851b07c83f014d75daaf8c0780bc27fa8da986
Kevin Turney <super996@optonline.net>
2018-05-23 11:28:29 -0400

    dependencies, dev and prod

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+// --------------  TO BE SCRAPPED FOR WEBPACK.CONFIG.JS ----------------
 
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,28 +13,32 @@ module.exports = {
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.scss$/,
-          use: [{
-            loader: 'style-loader'
-          }, {
-            loader: 'css-loader'
-          }, {
-            loader: 'sass-loader'
-          }]
-        },
-      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
-      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader']},
-      ]
-
-},
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
+    ],
+  },
   devServer: {
     contentBase: 'build',
     hot: true,
     inline: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
     new webpack.HotModuleReplacementPlugin(),
-    ],
-}
+  ],
+};
--- webpack.production.config.js
+++
@@ -1,1 +0,0 @@
-// In Progress 4/25/18

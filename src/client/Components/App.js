3d5b2fe52939a5b96cee3574af24f73c7c477f9a
Kevin Turney <super996@optonline.net>
2018-07-23 12:47:48 -0400

    new edit

--- src/client/Components/App.js
+++ src/client/Components/App.js
@@ -3,6 +3,6 @@ import { hot } from 'react-hot-loader';
 import styles from '../../stylesheets/main.scss';
 
 const App = () => {
-  return <div className="start">{'Hello Component hot'}</div>;
+  return <div className="start">{'new site'}</div>;
 };
 export default hot(module)(App);
